457e7b67293f6404e72bdd0a85aa9db476e76870
Riley Wilkinson <quintbro@gmail.com>
2023-11-01 14:52:18 -0600

    add data imputation.R

---
+++ dataimputation.R
@@ -0,0 +1,42 @@
+library(tidymodels)
+library(tidyverse)
+
+
+# setwd("C:/Users/rileyw/Ghosts-Ghouls-and-Goblins/")
+
+
+missing <- read_csv("trainWithMissingValues.csv")
+train <- read_csv("train.csv")
+
+is.na(missing$bone_length) %>%
+  sum()
+missing$rotting_flesh %>%
+  is.na() %>%
+  sum()
+missing$hair_length %>%
+  is.na() %>%
+  sum()
+missing$has_soul %>%
+  is.na() %>%
+  sum()
+missing$color %>%
+  is.na() %>%
+  sum()
+
+missing <- missing %>% select(-id)
+
+
+missing_recipe <- recipe(type ~ ., data = missing) %>%
+  step_string2factor(c("color", "type")) %>%
+  step_impute_knn(hair_length) %>%
+  step_impute_knn(rotting_flesh) %>%
+  step_impute_knn(bone_length)
+
+
+prep(missing_recipe) %>%
+  bake(new_data = missing) -> imputed
+
+train <- train %>% select(-id)
+rmse_vec(train[is.na(missing)], imputed[is.na(missing)])
+imputed[is.na(missing)]
+
